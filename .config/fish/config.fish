59105e09afbacddd9e3c2878d474837918f4e350
ilyasveil <3448766+ilyasveil@users.noreply.github.com>
2021-01-13 15:09:34 +1100

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,3 @@
+# starship init fish | source         # Enable starship prompt
 set fish_greeting                     # Disable greeting
 set -x PATH $HOME/.cargo/bin $PATH    # Add Rust binaries to PATH
