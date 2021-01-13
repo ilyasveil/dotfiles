ec65b9b85c2c4dc9b00b6216fad8eaf49eed48a1
ilyasveil <3448766+ilyasveil@users.noreply.github.com>
2021-01-13 15:44:29 +1100

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
-# starship init fish | source         # Enable starship prompt
-set fish_greeting                     # Disable greeting
-set -x PATH $HOME/.cargo/bin $PATH    # Add Rust binaries to PATH
+set fish_greeting # Disable greeting
+set -x PATH $HOME/.cargo/bin $PATH # Add Rust binaries to PATH
+set -x STARSHIP_CONFIG $HOME/.config/starship/starship.toml # Put starship config where everything else is...
+# starship init fish | source # Enable starship prompt
